0928b4e2352694b57c45adbe6c8e0ab45eab0b41
Jimolle <jimolofsamuel@gmail.com>
2016-04-15 11:35:26 +0200

    Serverside attribute validation maxwords

--- MVCDesignmonster.WebUI/Controllers/HomeController.cs
+++ MVCDesignmonster.WebUI/Controllers/HomeController.cs
@@ -4,15 +4,39 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVCDesignmonster.Logging;
+using MVCDesignmonster.WebUI.ViewModels;
 
 namespace MVCDesignmonster.WebUI.Controllers
 {
     [LoggingFilter]
     public class HomeController : Controller
     {
+        private TestViewModel _testViewModel;
+
+        public HomeController()
+        {
+            _testViewModel = new TestViewModel()
+            {
+                Text = null,
+                MaxWords = 3 //OBS! HÅRDKODAT även i TestViewModel...
+            };
+        }
+
         public ActionResult Index()
         {
-            return View();
+            return View(_testViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Index(TestViewModel inputTestViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _testViewModel.Text = inputTestViewModel.Text;
+                return View(_testViewModel);
+            }
+
+            return View(inputTestViewModel);
         }
 
         public ActionResult About()
---
+++ MVCDesignmonster.WebUI/ViewModels/TestViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using MVCDesignmonster.WordCount;
+
+namespace MVCDesignmonster.WebUI.ViewModels
+{
+    public class TestViewModel
+    {
+        [Required]
+        [WordCount(3, ErrorMessage = "Max 3 words in {0}")]
+        public string Text { get; set; }
+
+        public int MaxWords { get; set; }
+    }
+}
